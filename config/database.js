a41ab31549f9259f141ee13b8b5bcaf8ac364656
The Quill Mac <thequillmac@Thes-MacBook-Pro.local>
2019-10-28 19:57:52 -0400

    changed db from test to moms-cookbook

--- config/database.js
+++ config/database.js
@@ -1,1 +1,1 @@
-module.exports = {mongoURI: 'mongodb+srv://jcq5010:meghan33@momscookbook-dev-3ft08.mongodb.net/test?retryWrites=true&w=majority'}
+module.exports = {mongoURI: 'mongodb+srv://jcq5010:meghan33@momscookbook-dev-3ft08.mongodb.net/moms-cookbook?retryWrites=true&w=majority'}
