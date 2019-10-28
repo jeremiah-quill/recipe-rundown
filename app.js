32d10f4227620618cacab05d77257e1f186d1d3e
The Quill Mac <thequillmac@Thes-MacBook-Pro.local>
2019-10-28 17:25:17 -0400

    added url encoding

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
+
+app.use(express.urlencoded({ extended: false }))
+
 app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
