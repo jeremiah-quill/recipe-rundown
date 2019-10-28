eb7fd9341e5e1562babc275e1ca1783d22b4aad7
The Quill Mac <thequillmac@Thes-MacBook-Pro.local>
2019-10-28 17:16:01 -0400

    fixed authentication issue

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const users = require('./routes/users');
 
 
 
-// require('./config/passport')(passport); 
+require('./config/passport')(passport); 
 const db = require('./config/database')
 
 // Connect to Mongoose
--- config/passport.js
+++ config/passport.js
@@ -3,35 +3,34 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 // Load user model
-// const User = mongoose.model('users');
+const User = mongoose.model('users');
 
-// module.exports = function(passport){
-//     passport.use(new LocalStrategy({
-//         usernameField: 'email'}, (email, password, done) => {
-//             // Match email
-//             User.findOne({
-//                 email:email,
-//             }).then(user => {
-//                 if(!user) {
-//                     return done(null, false, {message: 'Email not registered'})
-//                 }
-//                 // Match password
-//                 bcrypt.compare(password, user.password, (err, isMatch) => {if(err) throw err;
-//                 if(isMatch){
-//                     return done(null, user);
-//                 } else {
-//                     return done(null, false, {message: 'Incorrect Password'});
-//                 }})
-//             })
-//         }));
+module.exports = function(passport){
+    passport.use(new LocalStrategy((username, password, done) => {
+            // Match email
+            User.findOne({
+                username:username,
+            }).then(user => {
+                if(!user) {
+                    return done(null, false, {message: 'Username not registered'})
+                }
+                // Match password
+                bcrypt.compare(password, user.password, (err, isMatch) => {if(err) throw err;
+                if(isMatch){
+                    return done(null, user);
+                } else {
+                    return done(null, false, {message: 'Incorrect Password'});
+                }})
+            })
+        }));
 
-//         passport.serializeUser(function(user, done) {
-//             done(null, user.id);
-//           });
+        passport.serializeUser(function(user, done) {
+            done(null, user.id);
+          });
           
-//           passport.deserializeUser(function(id, done) {
-//             User.findById(id, function(err, user) {
-//               done(err, user);
-//             });
-//           });
-// }
+          passport.deserializeUser(function(id, done) {
+            User.findById(id, function(err, user) {
+              done(err, user);
+            });
+          });
+}
